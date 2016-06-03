d4cd962d0e5fff15d8090396cd2c292d19fec3ea
Bryan Neva <bryan.neva@gmail.com>
2016-06-03 14:04:31 -0400

    Add all images and improve interactivity and design

--- projects/atmospheric-lifecycle/javascripts/scripts.js
+++ projects/atmospheric-lifecycle/javascripts/scripts.js
@@ -1,16 +1,28 @@
 $(function() {
     $('.atml-description--title').click(function(e) {
+        
         e.preventDefault();
+        
         var $this = $(this);
         var $contentId = $this.data('content');
         var $content = $this.parent().next('div');
         var $descriptions = $('.atml-description--content');
+        
+        var $listItems = $('.atml-description--list-item') 
+        var $selectedListWrapper = $this.closest('li');
+
         var $images = $('.atml-image--image');
         var $relatedImage = $('.atml-image--image[data-content="' + $contentId + '"]');
-        console.log($relatedImage[0]);
+        
+
         $descriptions.removeClass('active')
         $images.removeClass('active')
         $content.addClass('active')
         $relatedImage.addClass('active');
+
+        $listItems.removeClass('active');
+        $selectedListWrapper.addClass('active');
+
+        return false;
     });
 });
