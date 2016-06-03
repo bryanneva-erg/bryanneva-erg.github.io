44e7d8375e2247ca955cbdf081a35520be84bfdf
Bryan Neva <bryan.neva@gmail.com>
2016-06-03 14:52:39 -0400

    Update page titles depending on location. Expand area of highlight

--- projects/atmospheric-lifecycle/javascripts/scripts.js
+++ projects/atmospheric-lifecycle/javascripts/scripts.js
@@ -7,12 +7,11 @@ $(function() {
         var $contentId = $this.data('content');
         var $content = $this.parent().next('div');
         var $descriptions = $('.atml-description--content');
-        
         var $listItems = $('.atml-description--list-item') 
         var $selectedListWrapper = $this.closest('li');
-
         var $images = $('.atml-image--image');
         var $relatedImage = $('.atml-image--image[data-content="' + $contentId + '"]');
+        var $title = $('.atml-header h1');
         
 
         $descriptions.removeClass('active')
@@ -22,6 +21,26 @@ $(function() {
 
         $listItems.removeClass('active');
         $selectedListWrapper.addClass('active');
+
+        console.log('Content ID', $contentId);
+        switch ($contentId) {
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                $title.text('Loss Pathways')
+                break;
+            case 1:
+            case 8:
+            case 2:
+            case 3:
+            default:
+                $title.text('Atmospheric Lifetimes')
+                break;
+
+
+            
+        }
 
         return false;
     });
